26623e0137d521c685023be0f032ebeb64591f80
trattoos23 <tomyshow23@gmail.com>
2020-10-03 08:50:42 -0500

    nuevo proyecto

---
+++ src/Pantallas/AddProducts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+export default function AddProduct() {
+    return(
+        <View>
+            <text>Soy la pantalla de agregar productos</text>
+        </View>
+    )
+}
---
+++ src/Pantallas/ListarProductos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ListarProductos() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
